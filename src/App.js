56411b2f4e2b316ce5943c0feba46d796cb4a887
dhruv <dhruvbhatnagar10@gmail.com>
2020-01-10 21:13:09 +0530

    work in progress: home page design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
+import { ThemeProvider } from '@chakra-ui/core';
 
 // Pages
 import HomePage from './HomePage';
@@ -8,20 +9,22 @@ import VizCartographerJourney from './VizCartographerJourney';
 import VizDataGlobe from './VizDataGlobe';
 
 const App = () => (
-  <Router>
-    <Route exact path="/" component={HomePage} />
-    <Route exact path="/poi-analytics" component={VizPOIAnalytics} />
-    <Route
-      exact
-      path="/cartographer-journey"
-      component={VizCartographerJourney}
-    />
-    <Route
-      path="/cartographer-journey/:id"
-      component={VizCartographerJourney}
-    />
-    <Route exact path="/data-globe" component={VizDataGlobe} />
-  </Router>
+  <ThemeProvider>
+    <Router>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/poi-analytics" component={VizPOIAnalytics} />
+      <Route
+        exact
+        path="/cartographer-journey"
+        component={VizCartographerJourney}
+      />
+      <Route
+        path="/cartographer-journey/:id"
+        component={VizCartographerJourney}
+      />
+      <Route exact path="/data-globe" component={VizDataGlobe} />
+    </Router>
+  </ThemeProvider>
 );
 
 export default App;
--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { store } from '../global-store';
 import fetchViz3Data from '../utils/helper';
+import { Box, Image, Flex } from '@chakra-ui/core';
 
 import Card from './components/Card';
 import './index.css';
@@ -15,50 +16,32 @@ const HomePage = () => {
   useEffect(() => {
     console.log('state0');
     store.loading = true;
-    fetchViz3Data().then((data) => {
+    fetchViz3Data().then(data => {
       store.loading = false;
       localStorage.setItem('viz3data', JSON.stringify(data));
     });
   }, []);
+
+  const vizes = [vizGlobeImage, vizCartoJourneyImage, vizPOIImage];
 
   return (
     <div>
       <Helmet>
         <title>FOAMViz Project</title>
       </Helmet>
-      <div className="head-container">
-        <h1 className="main-title">foam-viz</h1>
-        <h4 className="main-info">Data Visualization over FOAM's TCR data</h4>
-      </div>
-      <div className="body-container">
+      <div className="main">
+        <div className="body-container">
+          <img
+            src="https://via.placeholder.com/150/0000FF/808080?Text=Digital.com"
+            className="background"
+          />
+        </div>
         <div className="card-container">
-          <div>
-            <Link to="/poi-analytics" className="card-link">
-              <Card
-                imageSrc={vizPOIImage}
-                title="POI Analytics"
-                description="This visualization shows aggreagated POIs according to density or staked value"
-              />
-            </Link>
-          </div>
-          <div>
-            <Link to="/cartographer-journey" className="card-link">
-              <Card
-                imageSrc={vizCartoJourneyImage}
-                title="Cartographer's Journey"
-                description="This visualization shows journey of a cartographer in terms of their plotting on the map"
-              />
-            </Link>
-          </div>
-          <div>
-            <Link to="/data-globe" className="card-link">
-              <Card
-                imageSrc={vizGlobeImage}
-                title="Data Globe"
-                description="This visualization shows all POIs since inception of FOAM on a globe for a bird eye's view"
-              />
-            </Link>
-          </div>
+          {vizes.map(v => (
+            <Box as="span" px="4" py="4">
+              <Image src={v} alt="img" />
+            </Box>
+          ))}
         </div>
       </div>
     </div>
