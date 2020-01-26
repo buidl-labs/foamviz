d13466b8d98ea5c193977de4e443e670e5b2aabd
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-01-26 23:03:08 +0530

    Shifting to heroku server for fetching VizDataGlobe data

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,6 +1,6 @@
 async function fetchViz3Data() {
-  const res = await fetch('https://map-api-direct.foam.space/poi/filtered?swLng=-180&swLat=-90&neLng=180&neLat=90&limit=10000&offset=0&sort=oldest').then((res) => res.json());
-  return res;
+  const res = await fetch('https://foamviz-api.herokuapp.com/').then((res) => res.json());
+  return JSON.parse(res);
 }
 
 export default fetchViz3Data;
