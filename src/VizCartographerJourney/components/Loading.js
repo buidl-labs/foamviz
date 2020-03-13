33c179a4728a2762e7513b5ededa7c4bf9a8777e
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-03-13 16:20:08 +0530

    Fixed: Shuffling of Loader color btw black/ grey

--- src/VizCartographerJourney/components/Loading.js
+++ src/VizCartographerJourney/components/Loading.js
@@ -7,34 +7,35 @@ const Loading = (props) => {
 
   return (
     <div
-    style={{
-      position: 'absolute',
-      zIndex: 2,
-      height: '100vh',
-      width: '100vw',
-      backgroundColor: 'rgba(0,0,0,0.5)',
-      transition: 'opacity 0.3s',
-      opacity: 1
-    }}
-  >
-    <div className="lds-ripple">
-      <div />
-      <div />
-    </div>
-    <h2
-      className="w-80"
       style={{
-    maxWidth: '428px',
-    color: 'white',
-    textAlign: 'center',
-    margin: 'auto',
-    marginTop: '30vh'
-  }}
->
-  Please wait, while we are fetching your data...
+        position: 'absolute',
+        zIndex: 2,
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: 'rgba(0,0,0)',
+        transition: 'opacity 0.3s',
+        opacity: 1
+      }}
+    >
+      <div className="lds-ripple">
+        <div />
+        <div />
+      </div>
+      <h2
+        className="w-80"
+        style={{
+          maxWidth: '428px',
+          color: 'white',
+          textAlign: 'center',
+          margin: 'auto',
+          marginTop: '32vh',
+          fontWeight: 'bold',
+          fontSize: '1.35rem'
+        }}
+      >
+        Please wait, while we are fetching your data...
     </h2>
-  </div >
-
+    </div >
   );
 };
 
