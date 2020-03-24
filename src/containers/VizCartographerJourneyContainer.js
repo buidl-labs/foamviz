3b67c6f19e2fe69fcaa48ad4fb4a3f5eb323230c
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-03-24 18:49:31 +0530

    Fixing: Routes with viz-cartographer-journey

--- src/containers/VizCartographerJourneyContainer.js
+++ src/containers/VizCartographerJourneyContainer.js
@@ -1,4 +1,5 @@
 import React, { lazy, Suspense } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Loader from '../common-utils/components/Loader';
 
 const VizCartographerJourney = lazy(() => import('../VizCartographerJourney'));
@@ -6,7 +7,17 @@ const VizCartographerJourney = lazy(() => import('../VizCartographerJourney'));
 const renderLoader = () => <Loader />;
 
 export default () => (
-    <Suspense fallback={renderLoader()}>
-        <VizCartographerJourney />
-    </Suspense>
+  <Suspense fallback={renderLoader()}>
+    <Router>
+      <Route
+        exact
+        path="/cartographer-journey"
+        component={VizCartographerJourney}
+      />
+      <Route
+        path="/cartographer-journey/:id"
+        component={VizCartographerJourney}
+      />
+    </Router>
+  </Suspense>
 );
